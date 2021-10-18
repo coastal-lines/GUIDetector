3bb2e0e39344896dee33ad5ae84c476a81a25551
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2021-10-18 15:28:34 +0300

    Add files via upload

--- Helpers/OCR/TesseractClass.py
+++ Helpers/OCR/TesseractClass.py
@@ -5,14 +5,12 @@ from Helpers.CommonMethods import CommonMethods
 class TesseractOCR():
 
     def GetTextFromImage(image):
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = r'c:\Temp\tesseract\tesseract.exe'
 
         #convert to negative
         image = ImageConverters.ConvertImageToNegative(image)
 
         text = pytesseract.image_to_string(image)
-        #print(text)
+        print(text)
         return text
-
-    def GetTextFromROI(image, x, y, w, h):
-        roi = CommonMethods.CropImage(x, y, w, h)
