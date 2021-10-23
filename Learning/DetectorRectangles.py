c39694d9d554db62cd1bb1768c217b82fa2f90bb
paul <piankin250@gmail.com>
2021-10-23 22:06:26 +0300

    opencv

--- Learning/DetectorRectangles.py
+++ Learning/DetectorRectangles.py
@@ -16,6 +16,13 @@ from Helpers.FeatureExtractors.Contours import Countours
 
 img = ImageLoaders.LoadImage(r'C:\Temp2\Flash\MyLabeling\CustomWindow.png')
 
+#паттерн был сделан с разрешением 1920*1200 монитора и имеет размер 420*200
+#текущий экран 1600*1200
+#находим все прямоугольники и сжимаем их до соответствующих пропорций т.е. подгоняя под 1920*1200
+#далее сравниваем попиксельно паттерн и каждый прямоугольник
+#будет множество вариантов ускорения
+#поискать как быть в случае частичного совпадения
+
 img_bw = ImageConverters.ConvertToBW(img)
 th = Threshold.AdaptiveThreshold(img_bw, 255, 11, 8)
 erosion = MorphologicalOperations.Erosion(th)
