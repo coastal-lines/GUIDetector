7919f59b0c3f56d4db002e480e488dc65f265eb6
paul <piankin250@gmail.com>
2021-10-24 23:23:47 +0300

    opencv

--- Learning/DetectorRectangles.py
+++ Learning/DetectorRectangles.py
@@ -14,11 +14,6 @@ from Helpers.MorphologicalOperations import MorphologicalOperations
 from Helpers.OCR.TesseractClass import TesseractOCR
 from Helpers.FeatureExtractors.Contours import Countours
 
-arr = set()
-#arr = {(100, "John"), (200, "Anna")}
-arr.add((300, "Lena"))
-print(arr)
-
 #паттерн был сделан с разрешением 1920*1200 монитора и имеет размер 420*200
 #текущий экран 1600*1200
 #находим все прямоугольники и сжимаем их до соответствующих пропорций т.е. подгоняя под 1920*1200
@@ -36,38 +31,44 @@ th = Threshold.AdaptiveThreshold(img_bw, 255, 11, 8)
 erosion = MorphologicalOperations.Erosion(th)
 blur = ImageFilters.Blur(erosion)
 contours, hierarchy = Countours.GetContours(blur)
-print(len(contours))
+#print(len(contours))
 
+range_value = 512
 def Comparator(pattern, roi):
     match_count = 0
-    for i in range(128):
-        for j in range(128):
+    for i in range(range_value):
+        for j in range(range_value):
             if(pattern[i][j] == roi[i][j]):
                 match_count+=1
 
     return match_count
 
-matches = []
-matches_set = set()
+temp = []
+count = 0
 for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
     point1 = (x, y)
     point2 = (x + w, y + h)
-    #cv2.rectangle(current_screen, point1, point2, (0, 255, 0), 1)
-    roi = CommonMethods.CropImageFromContour(current_screen, contour)
-    roi_bw = ImageConverters.ConvertToBW(roi)
-    pattern_bw = ImageConverters.ConvertToBW(pattern)
-    pattern_resized = CommonMethods.Resize(pattern_bw, 128, 128)
-    roi_resized = CommonMethods.Resize(roi_bw, 128, 128)
-    match = Comparator(pattern_resized, roi_resized)
-    print(match)
-    matches.append(match)
-    matches_set.add((match, contour))
+    if w > 600 and h > 600:
+        #cv2.rectangle(current_screen, point1, point2, (0, 255, 0), 1)
+        roi = CommonMethods.CropImageFromContour(current_screen, contour)
+        roi_bw = ImageConverters.ConvertToBW(roi)
+        pattern_bw = ImageConverters.ConvertToBW(pattern)
+        pattern_resized = CommonMethods.Resize(pattern_bw, range_value, range_value)
+        roi_resized = CommonMethods.Resize(roi_bw, range_value, range_value)
+        match = Comparator(pattern_resized, roi_resized)
+        temp.append((match, contour))
+        print(match)
+        #matches.append(match)
+        #matches_set.add(contour)
+        #count += 1
+        #if count > 5:
+        #    break
 
-matches.sort(reverse=True)
-print("max_match: ")
-print(matches[0])
-print(matches[1])
-print(matches[2])
-
-#CommonMethods.ShowImage(pattern_resized)
+print("========")
+sorted_multi_list = sorted(temp, key=lambda x: x[0], reverse=True)
+print(sorted_multi_list[0])
+Countours.DrawRectangle(sorted_multi_list[0][1],current_screen)
+Countours.DrawRectangle(sorted_multi_list[1][1],current_screen)
+Countours.DrawRectangle(sorted_multi_list[2][1],current_screen)
+CommonMethods.ShowImage(current_screen)
