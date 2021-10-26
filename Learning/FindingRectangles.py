10fbc601e24dec53e03cb02718311972b017c5bb
paul <piankin250@gmail.com>
2021-10-27 01:35:22 +0300

    opencv

--- Learning/FindingRectangles.py
+++ Learning/FindingRectangles.py
@@ -15,21 +15,27 @@ from Helpers.OCR.TesseractClass import TesseractOCR
 from Helpers.FeatureExtractors.Contours import Countours
 
 img = ImageLoaders.LoadImage(r'C:\Temp2\Flash\MyLabeling\CustomWindow.png')
+img_bw = ImageConverters.ConvertToBW(img)
+sharp = ImageFilters.Sharp(img_bw)
+erosion = MorphologicalOperations.Erosion(sharp)
+blur = ImageFilters.Blur(erosion)
 
-def SharpAndNegative(img):
-    img_bw = ImageConverters.ConvertToBW(img)
-    negative = cv2.bitwise_not(img_bw)
-    contours, hierarchy = Countours.GetContours(negative)
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        point1 = (x, y)
-        point2 = (x + w, y + h)
-        #if(h > 300):
-        cv2.rectangle(img, point1, point2, (0, 255, 0), 1)
-    print(len(contours))
-    return img
+ddept=cv2.CV_16S
+x = cv2.Sobel(blur, ddept, 1,0, ksize=3, scale=10)
+y = cv2.Sobel(blur, ddept, 0,1, ksize=3, scale=10)
+absx= cv2.convertScaleAbs(x)
+absy = cv2.convertScaleAbs(y)
+edge = cv2.addWeighted(absx, 0.5, absy, 0.5, 1)
 
-img=SharpAndNegative(img)
-image = cv2.resize(img, (2400, 800))
+contours, hierarchy = Countours.GetContoursByCanny(edge,220,255)
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    point1 = (x, y)
+    point2 = (x + w, y + h)
+    if w > 300:
+        cv2.rectangle(img, point1, point2, (255, 255, 0), 3)
+
+th = Threshold.AdaptiveThreshold(blur,255,11,8)
+image = cv2.resize(th, (1400, 800))
 cv2.imshow("", image)
 cv2.waitKey(0)
