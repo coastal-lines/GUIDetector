09a24b7edc7f8140cc4c14a9e10d5f09a9a4ef06
paul <piankin250@gmail.com>
2021-10-27 23:52:03 +0300

    opencv

--- Learning/FindingRectangles.py
+++ Learning/FindingRectangles.py
@@ -13,7 +13,11 @@ from Helpers.Filters.ImageFilters import ImageFilters
 from Helpers.MorphologicalOperations import MorphologicalOperations
 from Helpers.OCR.TesseractClass import TesseractOCR
 from Helpers.FeatureExtractors.Contours import Countours
+from Helpers.PatternMatching.PatternMatching import PatternMatching
 
+#11351 match
+pattern = ImageLoaders.LoadImage(r'C:\Temp2\Flash\MyLabeling\TestSimpleComparing\pattern2.png')
+pattern_bw = ImageConverters.ConvertToBW(pattern)
 img = ImageLoaders.LoadImage(r'C:\Temp2\Flash\MyLabeling\CustomWindow.png')
 img_bw = ImageConverters.ConvertToBW(img)
 sharp = ImageFilters.Sharp(img_bw)
@@ -21,21 +25,28 @@ erosion = MorphologicalOperations.Erosion(sharp)
 blur = ImageFilters.Blur(erosion)
 
 ddept=cv2.CV_16S
-x = cv2.Sobel(blur, ddept, 1,0, ksize=3, scale=10)
-y = cv2.Sobel(blur, ddept, 0,1, ksize=3, scale=10)
+x = cv2.Sobel(erosion, ddept, 1,0, ksize=3, scale=10)
+y = cv2.Sobel(erosion, ddept, 0,1, ksize=3, scale=10)
 absx= cv2.convertScaleAbs(x)
 absy = cv2.convertScaleAbs(y)
 edge = cv2.addWeighted(absx, 0.5, absy, 0.5, 1)
 
+range_value = 128
 contours, hierarchy = Countours.GetContoursByCanny(edge,220,255)
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
     point1 = (x, y)
     point2 = (x + w, y + h)
-    if w > 300:
-        cv2.rectangle(img, point1, point2, (255, 255, 0), 3)
+    cv2.rectangle(img, point1, point2, (255, 255, 0), 3)
+    roi = CommonMethods.CropImage(img_bw, x, y, w, h)
 
-th = Threshold.AdaptiveThreshold(blur,255,11,8)
-image = cv2.resize(th, (1400, 800))
+    pattern_resized = CommonMethods.Resize(pattern_bw, range_value, range_value)
+    roi_resized = CommonMethods.Resize(roi, range_value, range_value)
+
+    p = PatternMatching.ComparePixelByPixel(pattern_resized,roi_resized)
+    print(p)
+
+th = Threshold.AdaptiveThreshold(erosion,255,11,8)
+image = cv2.resize(img, (1400, 800))
 cv2.imshow("", image)
 cv2.waitKey(0)
