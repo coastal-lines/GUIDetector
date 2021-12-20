833a66c09f8936e7d8b6460a77df05786dc13014
paul <piankin250@gmail.com>
2021-12-20 23:50:51 +0300

    opencv

--- Helpers/ActionsForElements.py
+++ Helpers/ActionsForElements.py
@@ -39,18 +39,20 @@ class ActionsForElements():
         if(scroll_p1 != None and scroll_p2 != None):
             self.ClickOnTheCentreOfTheElement(self, up_button_p1, up_button_p2)
 
-    def IsTheBottomOfScroll(self, roi_bw, scroll_p1, scroll_p2, down_button_p1, down_button_p2):
-        pass
+    def IsTheBottomOfScroll(self, roi_bw, up_button, down_button):
+        scroll_p1, scroll_p2, up_button_p1, up_button_p2, down_button_p1, down_button_p2 = self.FindScrollArea(self, roi_bw, up_button, down_button)
+
         # save start view of scrollbar
-        #scroll_before_click = CommonMethods.CropImageByPoints(roi_bw, scroll_p1, scroll_p2)
-        #score_before_click = PatternMatching.ComparePixelByPixel(scroll_before_click, scroll_before_click)
+        scroll_before_click = CommonMethods.CropImageByPoints(roi_bw, scroll_p1, scroll_p2)
 
         # click down
-        #self.ClickOnTheCentreOfTheElement(self, down_button_p1, down_button_p2)
+        self.ClickOnTheCentreOfTheElement(self, down_button_p1, down_button_p2)
 
         # check bottom condition
-        #scroll_after_click = CommonMethods.CropImageByPoints(roi_bw, scroll_p1, scroll_p2)
-        #score_after_click = PatternMatching.ComparePixelByPixel(scroll_before_click, scroll_after_click)
+        scroll_after_click = CommonMethods.CropImageByPoints(roi_bw, scroll_p1, scroll_p2)
+
+        # 
+        score = PatternMatching.ComparePixelByPixel(scroll_before_click, scroll_after_click)
         #if(score_after_click != score_before_click):
         #    return True
 
