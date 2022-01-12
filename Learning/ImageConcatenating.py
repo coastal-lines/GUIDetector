5aa893d6d62de49220ce632fbe7a65b56c729815
paul <piankin250@gmail.com>
2022-01-12 23:53:20 +0300

    opencv

---
+++ Learning/ImageConcatenating.py
@@ -0,0 +1,24 @@
+from Helpers.Json import JsonHelper
+import numpy as np
+import pandas as pd
+from Helpers.ImageLoaders import ImageLoaders
+from Helpers.CommonMethods import CommonMethods
+from BusinessTasks.Tasks import Tasks
+import cv2
+from decimal import Decimal
+from Helpers.FeatureExtractors.MSER import MSER
+from Helpers.Threshold import Threshold
+from Helpers.ImageConverters import ImageConverters
+from Helpers.Filters.ImageFilters import ImageFilters
+from Helpers.MorphologicalOperations import MorphologicalOperations
+from Helpers.OCR.TesseractClass import TesseractOCR
+from Helpers.FeatureExtractors.Contours import Contours
+from Helpers.Json.LabeledData import OriginalElement, LabeledData
+from Helpers.PatternMatching.PatternMatching import PatternMatching
+from Helpers.ActionsForElements import ActionsForElements
+import pytesseract
+
+img1 = ImageLoaders.LoadBWImage()
+img2 = ImageLoaders.LoadBWImage()
+
+vis = np.concatenate((img1, img2), axis=0)
