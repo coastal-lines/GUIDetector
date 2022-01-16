4a4f0e62fd53c6f06b404560eecc3da30fe5a987
paul <piankin250@gmail.com>
2022-01-17 01:03:08 +0300

    gui labeling

---
+++ Learning/ImageConcatenating3.py
@@ -0,0 +1,24 @@
+from Helpers.Json import JsonHelper
+import numpy as np
+import pandas as pd
+from Helpers.ImageLoaders import ImageLoaders
+from Helpers.CommonMethods import CommonMethods
+from BusinessTasks.Tasks import Tasks
+import cv2
+from decimal import Decimal
+from Helpers.FeatureExtractors.MSER import MSER
+from Helpers.Threshold import Threshold
+from Helpers.ImageConverters import ImageConverters
+from Helpers.Filters.ImageFilters import ImageFilters
+from Helpers.MorphologicalOperations import MorphologicalOperations
+from Helpers.OCR.TesseractClass import TesseractOCR
+from Helpers.FeatureExtractors.Contours import Contours
+from Helpers.Json.LabeledData import OriginalElement, LabeledData
+from Helpers.PatternMatching.PatternMatching import PatternMatching
+from Helpers.ActionsForElements import ActionsForElements
+import pytesseract
+
+#алгоритм записал в черри
+
+img1 = ImageLoaders.LoadBWImage(r"C:\Temp\Photos\Subjects\3.bmp")
+img2 = ImageLoaders.LoadBWImage(r"C:\Temp\Photos\Subjects\4.bmp")
