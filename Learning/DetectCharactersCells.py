68e5bdcf035985f5f1e9aae1033728eb9d9000e0
paul <piankin250@gmail.com>
2022-01-22 22:24:06 +0300

    tesseract

--- Learning/DetectCharactersCells.py
+++ Learning/DetectCharactersCells.py
@@ -18,8 +18,8 @@ from Helpers.PatternMatching.PatternMatching import PatternMatching
 from Helpers.ActionsForElements import ActionsForElements
 import pytesseract
 
-list_subject1 = ImageLoaders.LoadBWImage("C:\Temp\Photos\data\list_subjects8.bmp")
-th, threshed = cv2.threshold(list_subject1, 127, 255, cv2.THRESH_BINARY_INV)
+user_image = ImageLoaders.LoadBWImage("C:\Temp\Photos\data\list_subjects8.bmp")
+th, threshed = cv2.threshold(user_image, 127, 255, cv2.THRESH_BINARY_INV)
 
 #т.е. вся матрица сжимается в вектор по горизонтали (все строки сжимаются в одну). при этом значения суммируются и делятся на число столбцов. т.о. получается среднее значение для столбца
 hist_x = cv2.reduce(threshed, 0, cv2.REDUCE_AVG)
@@ -31,7 +31,7 @@ hist_y_reshaped = hist_y.reshape(-1)
 #CommonMethods.ShowImage(hist)
 
 th = 2
-H,W = list_subject1.shape[:2]
+H,W = user_image.shape[:2]
 # обходим все горизонтальные точки
 # решение в одну строчку
 #uppers2 = [y for y in range(H-1) if hist_reshaped[y]<=th and hist_reshaped[y+1]>th]
@@ -43,13 +43,20 @@ for i in range(W - 1):
     if(hist_x_reshaped[i] <= th and hist_x_reshaped[i + 1] > th):
         uppers_x.append(i)
 
-uppers_y = []
+#нужно брать в расчёт только первую и последнюю линии т.к. между ними могут быть ложные совпадения, например точки и т.п.
+uppers_y_temp = []
 for i in range(H - 1):
     if(hist_y_reshaped[i] <= th and hist_y_reshaped[i + 1] > th):
-        uppers_y.append(i)
+        uppers_y_temp.append(i)
+for i in range(H - 1):
+    if(hist_y_reshaped[i] >= th and hist_y_reshaped[i + 1] < th):
+        uppers_y_temp.append(i)
+uppers_y = []
+uppers_y.append(uppers_y_temp[0])
+uppers_y.append(uppers_y_temp[-1])
 
 # содаём новое изображение
-finish_image = cv2.cvtColor(list_subject1, cv2.COLOR_GRAY2BGR)
+finish_image = cv2.cvtColor(user_image, cv2.COLOR_GRAY2BGR)
 
 # отрисовываем линии по полученным точкам максиумов
 for i in uppers_x:
@@ -58,6 +65,7 @@ for i in uppers_x:
 for i in uppers_y:
     cv2.line(finish_image, (0, i), (W, i), (255, 0, 0), 1)
 
+#CommonMethods.ShowImageWithOriginalSize(finish_image)
 # насамомделе ничего рисовать не нужно, нужны координаты полученных прямоугольников
 # т.е. 1й прямоугольник это пространство между линией 1 и линией2
 # плюс нужно немного сдвигать прямоугольник - текст не совсем ровно в него помещается
@@ -72,7 +80,8 @@ averageW = int(np.average(allWidthBetweenLetters))
 charactersRegions = []
 for i in range(len(uppers_x) - 1):
     p1 = (uppers_x[i], uppers_y[0])
-    p2 = (uppers_x[i + 1], uppers_y[1] + 5)
+    #p2 = (uppers_x[i + 1], uppers_y[1] + 5)
+    p2 = (uppers_x[i + 1], uppers_y[1])
     #cv2.rectangle(list_subject1, p1, p2, (0, 0, 0), 1)
     charactersRegions.append([p1, p2])
 
@@ -97,12 +106,14 @@ roiArray = []
 for i in range(len(charactersRegions)):
     p1 = charactersRegions[i][0]
     p2 = charactersRegions[i][1]
-    roi = CommonMethods.CropImageByPoints(list_subject1, p1, p2)
+    roi = CommonMethods.CropImageByPoints(user_image, p1, p2)
     img = np.zeros([roi.shape[0] + 20, roi.shape[1] + 20], dtype=np.uint8)
     img.fill(255)
     img[10:roi.shape[0] + 10, 10:roi.shape[1] + 10] = roi
     roiArray.append(img)
     #CommonMethods.ShowImageWithOriginalSize(img)
+
+#CommonMethods.ShowImageWithOriginalSize(user_image)
 
 #читаем каждый символ тессерактом
 for i in range(len(roiArray)):
@@ -110,4 +121,3 @@ for i in range(len(roiArray)):
     TesseractOCR.GetTextFromImage(roiArray[i])
 
 #CommonMethods.ShowImageWithOriginalSize(finish_image)
-#CommonMethods.ShowImageWithOriginalSize(list_subject1)
